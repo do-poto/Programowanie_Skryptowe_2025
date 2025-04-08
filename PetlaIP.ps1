ae18a28630dcc54ab010610c9bff2a67526c72df
cgdppl <dominikpotoczniak@gmail.com>
2025-04-08 15:15:19 +0200

    Organizational change

--- PetlaIP.ps1
+++
@@ -1,5 +0,0 @@
-for($i=0; $i -lt 10; $i++){
-   $random = Get-Random -Minimum 0 -Maximum 255
-    $ip = "192.168.1." + "$random"
-    Write-Host "$ip"
-}
--- Trojkat.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [int]$wysokosc,
-    [int]$dlugosc
-)
-
-[int]$pole = $wysokosc * $dlugosc * 0.5
-
-Write-Host "Pole to: $pole"
