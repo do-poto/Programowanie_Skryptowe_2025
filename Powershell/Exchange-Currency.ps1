f64fe896df11a981a45b4c072b2a2c81bdff4b44
cgdppl <dominikpotoczniak@gmail.com>
2025-04-28 22:35:25 +0200

    Date formatting and fetching now works

--- Powershell/Exchange-Currency.ps1
+++ Powershell/Exchange-Currency.ps1
@@ -1,31 +1,25 @@
-<#
 function Request-Rates(){
-    param([Parameter(Position=0; mandatory=$true)][String]$startCurrency, 
-    [String]$endCurrency)
+    #param([Parameter(Position=0; mandatory=$true)][String]$startCurrency)
+    
     $URI = "https://api.currencybeacon.com/v1/historical?"
-    $apiKey = ""
+    $apiKey = "api_key="
     $URI = $URI + $apiKey
 
-    $dateCall = Get-Date
-    $currentDate = $dateCall -Format "dd/mm/yyyy"
-    $pastDate = $dateCall.AddDays(-5) | -Format "dd/mm/yyyy"
+    $currentDate = Get-Date -Format "dd/mm/yyyy"
+    $pastDate = (Get-Date).AddDays(-5).toString("dd/mm/yyyy")
     
     $headers = @{}
     $headers.add("Content-Type", "application/json")
     
     $body = @{}
     $body.add("base", "PLN")
-    $body.add("stat_date", "")
-    $body.add("end_date", "")
+    $body.add("symbols", "EUR, USD")
+    $body.add("stat_date", $currentDate)
+    $body.add("end_date", $pastDate)
 
     $request = Invoke-WebRequest -URI $URI -Headers $headers -Body $body
 
     Write-Host $request
 }
-#>
 
-$currentDate = Get-Date -Format "dd/mm/yyyy"
-$pastDate = (Get-Date).AddDays(-5) | -Format "dd/mm/yyyy"
-
-Write-Host $currentDate
-Write-Host $pastDate
+Request-Rates
